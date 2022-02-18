f2beb7a176c4f73c65484e65ad09b8437c6eccb7
ciripel <nedelcu.alexandru@yahoo.com>
2022-02-18 09:51:21 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
+}
