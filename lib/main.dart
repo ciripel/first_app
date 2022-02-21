b2075a29fcc2075c0d8ac7c80c6688088407f9a2
ciripel <nedelcu.alexandru@yahoo.com>
2022-02-21 12:46:31 +0200

    ZcashTxexample

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
 import 'package:flutter_trust_wallet_core/trust_wallet_core_ffi.dart';
 
+// import 'package:first_app/base_example.dart';
+import 'package:convert/convert.dart';
+import 'package:flutter_trust_wallet_core/protobuf/bitcoin.pb.dart' as Bitcoin;
+import 'package:fixnum/fixnum.dart' as $fixnum;
+
 void main() {
   runApp(const MyApp());
 }
@@ -50,28 +55,94 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String mnemonic =
+      "horror select baby exile convince sunset outside vehicle write decade powder energy";
 
   late HDWallet wallet;
 
   @override
-  void initState() {
+  HDWallet initState() {
     FlutterTrustWalletCore.init();
-    String mnemonic =
-        "horror select baby exile convince sunset outside vehicle write decade powder energy";
+    super.initState();
+
     wallet = HDWallet.createWithMnemonic(mnemonic);
 
     print("Wallet Mnemonic: " + wallet.mnemonic());
     print("-------------------------");
-    print("Bitcoin Address: " +
-        wallet.getAddressForCoin(TWCoinType.TWCoinTypeBitcoin).toString());
+    print("Zcash Address: " +
+        wallet.getAddressForCoin(TWCoinType.TWCoinTypeZcash));
     print("Litecoin Address: " +
-        wallet.getAddressForCoin(TWCoinType.TWCoinTypeLitecoin).toString());
+        wallet.getAddressForCoin(TWCoinType.TWCoinTypeLitecoin));
     print("Ethereum Address: " +
-        wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum).toString());
+        wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum));
     print("Solana Address: " +
-        wallet.getAddressForCoin(TWCoinType.TWCoinTypeSolana).toString());
+        wallet.getAddressForCoin(TWCoinType.TWCoinTypeSolana));
     print("Stellar Address: " +
-        wallet.getAddressForCoin(TWCoinType.TWCoinTypeStellar).toString());
+        wallet.getAddressForCoin(TWCoinType.TWCoinTypeStellar));
+
+    int coin = TWCoinType.TWCoinTypeZcash;
+    final addressZec = wallet.getAddressForCoin(coin);
+    print(addressZec);
+    const toAddress = "t1RTNMRyJhc1UgfvrTSqFP46C5xuyxjJGeR";
+    const changeAddress = "t1RTNMRyJhc1UgfvrTSqFP46C5xuyxjJGeR";
+    final secretPrivateKeyBtc = wallet.getKeyForCoin(coin);
+    final signingInput = Bitcoin.SigningInput(
+      amount: $fixnum.Int64.parseInt('37000'),
+      hashType: BitcoinScript.hashTypeForCoin(coin),
+      toAddress: toAddress,
+      changeAddress: changeAddress,
+      byteFee: $fixnum.Int64.parseInt('10'),
+      coinType: coin,
+      utxo: [
+        Bitcoin.UnspentTransaction(
+          amount: $fixnum.Int64.parseInt('20000'),
+          outPoint: Bitcoin.OutPoint(
+            hash: hex
+                .decode(
+                    '1b23757cdc023b3ac9f033522abb9f845815b65cce1e25411e8ad950899c0e71')
+                .reversed
+                .toList(),
+            index: 0,
+            sequence: 4294967295,
+          ),
+          script: BitcoinScript.lockScriptForAddress(addressZec, coin)
+              .data()
+              .toList(),
+        ),
+        Bitcoin.UnspentTransaction(
+          amount: $fixnum.Int64.parseInt('20000'),
+          outPoint: Bitcoin.OutPoint(
+            hash: hex
+                .decode(
+                    '7611002ff116fad20ef12ad30010a07d5b25edf37209504dd42a6a4c5c27aa75')
+                .reversed
+                .toList(),
+            index: 0,
+            sequence: 4294967295,
+          ),
+          script: BitcoinScript.lockScriptForAddress(addressZec, coin)
+              .data()
+              .toList(),
+        ),
+      ],
+      privateKey: [
+        secretPrivateKeyBtc.data().toList(),
+      ],
+    );
+    final transactionPlan = Bitcoin.TransactionPlan.fromBuffer(
+        AnySigner.signerPlan(signingInput.writeToBuffer(), coin).toList());
+    // logger.d(
+    //     'availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
+    print(
+        'availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
+    signingInput.plan = transactionPlan;
+    signingInput.amount = transactionPlan.amount;
+    final sign = AnySigner.sign(signingInput.writeToBuffer(), coin);
+    final signingOutput = Bitcoin.SigningOutput.fromBuffer(sign);
+    print(hex.encode(signingOutput.encoded));
+    // logger.d(hex.encode(signingOutput.encoded));
+
+    return wallet;
   }
 
   void _incrementCounter() {
@@ -87,6 +158,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    String zcash = wallet.getAddressForCoin(TWCoinType.TWCoinTypeZcash);
+    String litecoin = wallet.getAddressForCoin(TWCoinType.TWCoinTypeLitecoin);
+    String ethereum = wallet.getAddressForCoin(TWCoinType.TWCoinTypeEthereum);
+    String solana = wallet.getAddressForCoin(TWCoinType.TWCoinTypeSolana);
+    String stellar = wallet.getAddressForCoin(TWCoinType.TWCoinTypeStellar);
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -119,12 +195,28 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Mnemonic:\n$mnemonic',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '\Zcash:\n$zcash',
+              // style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '\nLitecoin:\n$litecoin',
+              // style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '\nEthereum:\n$ethereum',
+              // style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '\nSolana:\n$solana',
+              // style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '\nStellar:\n$stellar',
+              // style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
@@ -132,7 +224,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_call),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
